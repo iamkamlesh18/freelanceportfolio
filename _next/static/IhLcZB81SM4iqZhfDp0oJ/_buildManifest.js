e13fe2bc6d7e38e1f28003add6cc0bfa155d17f7
Kamlesh Patil <65329245+iamkamlesh18@users.noreply.github.com>
2026-03-03 11:16:54 +0000

    Updates

--- _next/static/IhLcZB81SM4iqZhfDp0oJ/_buildManifest.js
+++ _next/static/IhLcZB81SM4iqZhfDp0oJ/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/freelanceportfolio//_next/:path+",
+        "destination": "/freelanceportfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
